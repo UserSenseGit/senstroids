8adeb3183d26027b958b34ad0bcceb25fa2d33d9
UserSenseGit <swennenmichael@gmail.com>
2026-02-21 22:42:58 +0100

    Triangle drawn

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
+
 def main():
     pygame.init()   
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     print(f"Starting Asteroids with pygame version: {pygame.__version__}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -19,6 +22,7 @@ def main():
                 return
             
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
   
